04f94f59dea4fed2b7dc04a59cc77fc4dbbdf365
AndereX-dev <andreas.osdal@hotmail.com>
2026-03-11 12:40:35 +0100

    Fixed CSS, HTML-files and started on the JS-file

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
-const copyBtn = document.getElementById("copyBtn");
-const passwordDisplay = document.getElementById("passwordDisplay")
+const characterAmountRange = document.getElementById("characterAmountRange");
+const characterAmounntNumber = document.getElementById("characterAmountNumber");
+
+function syncCharacterAmount(e) {
+    const value = e.target.value
+    characterAmountRange.value = value
+    characterAmountNumber.value = value
+}
+
+characterAmountRange.addEventListener("input", syncCharacterAmount);
+characterAmountNumber.addEventListener("input", syncCharacterAmount);
+
+const form = document.getElementById("passwordGeneratorForm");
+const includeUppercaseElement = document.getElementById("includeUppercase");
+const includeNumbersElement = document.getElementById("includeNumbers");
+const includeSymbolsElement = document.getElementById("includeSymbols");
+const passwordDisplay = document.getElementById("passwordDisplay");
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+    arrayFromLowToHigh(58, 64)
+).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeNumbers = includeNubersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+    
+    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+})
+
+function generatePassword(characterAmont, includeUppercase, includeNumbers, includeSymbols) {
+    let charCodes = LOWERCASE_CHAR_CODES
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+
+    const passwordCharacters = []
+    for (let i = 0; i < characterAmont; i++) {
+        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwordCharacters.join("")
+}
+
+function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+        array.push(i)
+    }
+    return array
+}
+
+const copyBtn = document.getElementById("copyBtn")
 
 copyBtn.addEventListener("click", () => {
-    const password = passwordDisplay.innerText;
-    if (!password || password === "Click generate...") return;
+    const password = passwordDisplay.innerText
+    if (!password || password === "Click Generate Password") return
 
     navigator.clipboard.writeText(password).then(() => {
-        alert("Password copied to clipboard!");
-    });
-});
-
+        const originalText = copyBtn.innerText
+        copyBtn.innerText = "Copied to clipboard!"
+        setTimeout(() => {
+            copyBtn.innerText = originalText
+        }, 2000)
+    })
+})
